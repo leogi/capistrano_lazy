f86c74af5dbbc6bcf3f0fc1d632f97edc495e0f7
Nghia Le Van <nghialv.bk@gmail.com>
2016-01-22 16:16:14 +0900

    database, secrets template

--- lib/capistrano_lazy/server_config.rb
+++ lib/capistrano_lazy/server_config.rb
@@ -1,3 +1,6 @@
+require "fileutils"
+require "securerandom"
+
 module CapistranoLazy
   class ServerConfig
     include TemplateHelper
@@ -9,11 +12,39 @@ module CapistranoLazy
     end
 
     def config!
+      prepare_deploy_directory
+      cp_database
+      cp_secret
       cp_nginx
       cp_unicorn
     end
 
     private
+    def cp_database
+      template File.expand_path("../templates/config/database.yml", __FILE__),
+        "config/database.yml", {
+          environment: @settings.environment,
+          database_name: @settings.database.name,
+          database_host: @settings.database.host,
+          database_username: @settings.database.username,
+          database_password: @settings.database.password
+        }
+
+      puts "\e[33mInstall\e[0m #{@settings.deploy.directory}/shared/config/database.yml"
+      FileUtils.cp "config/database.yml", "#{@settings.deploy.directory}/shared/config"
+    end
+
+    def cp_secret
+      template File.expand_path("../templates/config/secrets.yml", __FILE__),
+        "config/secrets.yml", {
+          environment: @settings.environment,
+          secret_key_base: SecureRandom.hex(64)
+        }
+
+      puts "\e[33mInstall\e[0m #{@settings.deploy.directory}/shared/config/secrets.yml"
+      FileUtils.cp "config/secrets.yml", "#{@settings.deploy.directory}/shared/config"
+    end
+
     def cp_nginx
       server_names = @settings.server_names
       server_names = ["_"] if server_names.nil? || server_names.compact.empty?
@@ -26,11 +57,12 @@ module CapistranoLazy
           unicorn_host: @settings.unicorn.host,
           unicorn_port: @settings.unicorn.port
         }
-        
+
       if File.exists? "/etc/nginx"
         unless File.exists? "/etc/nginx/sites-enabled"
           `sudo mkdir /etc/nginx/sites-enabled`
         end
+        puts "\e[33mInstall\e[0m /etc/nginx/sites-enabled/#{@settings.application}"
         `sudo mv nginx_#{@settings.application} /etc/nginx/sites-enabled/#{@settings.application}`
       else
         puts "Nginx is not found. Please install nginx first."
@@ -46,8 +78,26 @@ module CapistranoLazy
           deploy_user: @settings.deploy.user,
           pid_file: @settings.unicorn.pid_file
         }
+      puts "\e[33mInstall\e[0m /etc/init.d/#{@settings.application}"
       `sudo mv unicorn_#{@settings.application} /etc/init.d/`
       `sudo chmod +x /etc/init.d/unicorn_#{@settings.application}`
+    end
+
+    def prepare_deploy_directory
+      if !File.exists? "/var/www"
+        `sudo mkdir /var/www`
+      end
+
+      if !File.exists? @settings.deploy.directory
+        `sudo mkdir /var/www/#{@settings.application}`
+        `sudo chown #{@settings.deploy.user}:#{@settings.deploy.groupuser} /var/www/#{@settings.application}`
+        `chmod 755 /var/www/#{@settings.application}`
+      end
+
+      if !File.exists? "#{@settings.deploy.directory}/shared/config"
+        puts "\e[32mCreate\e[0m #{@settings.deploy.directory}/shared/config"
+        FileUtils.mkdir_p "#{@settings.deploy.directory}/shared/config"
+      end
     end
   end
 end
